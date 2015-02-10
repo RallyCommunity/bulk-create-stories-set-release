5060efb60a866dddbcbe4429b94e152efb46bd47
Nick Musaelian <nmusaelian@nmusaellian-mbr.f4tech.com>
2015-02-10 10:57:48 -0700

    refactored

--- App.js
+++ App.js
@@ -7,85 +7,75 @@ Ext.define('CustomApp', {
         labelWidth: 100,
         width: 300
     },
-    
+   
     onScopeChange: function() {
-        this._getRelease();
+        var that = this;
+        if (!this.down('#create-button')) {
+            this.add({
+                xtype  : 'rallybutton',
+                text   : 'create',
+                itemId: 'create-button',
+                handler: function() {
+                    that._getModel(); 
+                }
+            });
+        }
+        
     },
     
-    
-    _getRelease: function() {
-            var release = this.getContext().getTimeboxScope().record.get('_ref');
-            console.log('release',release);
-            
-            if (!this.down('#b2')) {
-                 var that = this;
-                 var cb = Ext.create('Ext.Container', {
-            
-                items: [
-                    {
-                        xtype  : 'rallybutton',
-                        text      : 'create',
-                        id: 'b2',
-                        handler: function() {
-                            that._getModel(release); 
-                        }
-                    }
-                        
-                    ]
-                });
-            this.add(cb);
-            }
-        },
-        
-
-    _getModel: function(release){
-            var that = this;
-            Rally.data.ModelFactory.getModel({
-                type: 'UserStory',
-                success: function(model) {  //success on model retrieved
-                    var count = 3;
-                    for(var i=1;i<count;i++){
-                        that._model = model;
-                        var story = Ext.create(model, {
-                            Name: 'story ' + i
-                        });
+    _getModel: function(){
+        var that = this;
+        Rally.data.ModelFactory.getModel({
+            type: 'UserStory',
+            success: function(model) {  //success on model retrieved
+                var count = 3;
+                for(var i=1;i<count;i++){
+                    that._model = model;
+                    var story = Ext.create(model, {
+                        Name: 'story ' + i
+                    });
                     story.save({
                         callback: function(result, operation) {
                             if(operation.wasSuccessful()) {
                                 console.log("_ref",result.get('_ref'), ' ', result.get('Name'));
                                 that._record = result;
-                                that._readAndUpdate(release);
+                                that._readAndUpdate();
                             }
                             else{
                                 console.log("?");
                             }
                         }
                     });
-                    }
-                    
                 }
-            });
-        },
+                
+            }
+        });
         
-        _readAndUpdate:function(release){
-            var id = this._record.get('ObjectID');
-            console.log('OID', id);
-            this._model.load(id,{
-                fetch: ['Name', 'FormattedID', 'Release'],
-                callback: function(record, operation){
-                    console.log('Release prior to update:', record.get('Release'));
-                    record.set('Release', release);
-                    record.save({
-                        callback: function(record, operation) {
-                            if(operation.wasSuccessful()) {
-                                console.log('Release after update..', record.get('Release'));
-                            }
-                            else{
-                                console.log("problem..");
-                            }
-                        }
-                    });
+            
+    },
+        
+    _readAndUpdate:function(){
+        var id = this._record.get('ObjectID');
+        var release = this.getContext().getTimeboxScope().record;
+        console.log('OID', id);
+        this._model.load(id,{
+            fetch: ['Name', 'FormattedID', 'Release'],
+            callback: function(record, operation){
+                console.log('Release prior to update:', record.get('Release'));
+                if(release){
+                    record.set('Release', release.get('_ref'));
                 }
-            });
-        }
+                record.save({
+                    callback: function(record, operation) {
+                        if(operation.wasSuccessful()) {
+                            console.log('Release after update..', record.get('Release'));
+                        }
+                        else{
+                            console.log("problem..");
+                        }
+                    }
+                });
+            }
+        });
+    }
 });
